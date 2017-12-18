9a32aba4445647e76be4a493e25d8dc70dd0d702
unknown <nyounaki@gmail.com>
2017-12-17 20:54:14 -0500

    ADd Message Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -14,17 +14,21 @@ hero: Hero = {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
   
    
 selectedHero: Hero;
-
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
 } 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
